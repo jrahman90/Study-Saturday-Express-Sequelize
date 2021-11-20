2c086da128e7fc89bfbf89736796130b237197fe
Jalilur Rahman <jrahman90.work@gmail.com>
2021-11-20 11:09:07 -0500

    as far as i did

--- db/models/student.js
+++ db/models/student.js
@@ -1,6 +1,31 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student',{
+    firstName:
+    {
+        type: Sequelize.STRING
+    },
+    lastName:
+    {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email:
+    {
+        type:Sequelize.STRING,
+        allowNull: false,
+        validate:{
+            isEmail: true
+        }
+    }
+});
+
+//create a before create hook
+Student.beforeCreate(user =>{
+    let arr = user.firstName.split('')
+    arr[0] = arr[0].toUpperCase
+    user.firstNam = arr.join('')
+})
 
 module.exports = Student;
